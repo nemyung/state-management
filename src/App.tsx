6162c04d4e7494edd0de538c4b1e673d8fa7c0be
semyung oh <semyung.dev@gmail.com>
2023-08-12 20:17:37 +0900

    module state - add uSESWS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // import NaiveApproach from "./NaiveApproach";
 // import SubscriptionApproach from "./SubscriptionApproach";
 // import SU
-import SubscriptionApproachWithSelector from "./SubscriptionApproachWithSelector";
+// import SubscriptionApproachWithSelector from "./SubscriptionApproachWithSelector";
+import SubscriptionApproachWithSelector from "./SubscriptionApproachWithBultinReactHook";
 
 function App() {
   return (
