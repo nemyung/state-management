1cf39cf7018360d1dfee6f48f01906ef328d72b5
semyung oh <semyung.dev@gmail.com>
2023-08-12 20:40:22 +0900

    combining context with module state subscription mechanism

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-// import NaiveApproach from "./NaiveApproach";
-// import SubscriptionApproach from "./SubscriptionApproach";
-// import SU
-// import SubscriptionApproachWithSelector from "./SubscriptionApproachWithSelector";
-import SubscriptionApproachWithSelector from "./SubscriptionApproachWithBultinReactHook";
+import { StoreContextProvider, useSelector, useSetState } from "./module-state";
+
+function ExampleComponent() {
+  const count = useSelector((state) => state.count);
+  const setState = useSetState();
+  const inc = () => {
+    setState((prev) => ({ ...prev, count: prev.count + 1 }));
+  };
+  return (
+    <div>
+      count: {count} <button onClick={inc}>++</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-      {/* <NaiveApproach /> */}
-      {/* <SubscriptionApproach /> */}
-      <SubscriptionApproachWithSelector />
+      <h1>1. using default store</h1>
+      <ExampleComponent />
+      <ExampleComponent />
+
+      <StoreContextProvider initialState={{ count: 100, text: "" }}>
+        <h1>2. using store porvider. default count is 100</h1>
+        <ExampleComponent />
+        <ExampleComponent />
+        <StoreContextProvider initialState={{ count: 1000, text: "" }}>
+          <h1>2. using store porvider. default count is 1000</h1>
+          <ExampleComponent />
+          <ExampleComponent />
+        </StoreContextProvider>
+      </StoreContextProvider>
     </>
   );
 }
