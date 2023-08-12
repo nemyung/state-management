3e008c847e0727509f2360bca81c0f8eaa77a99c
semyung oh <semyung.dev@gmail.com>
2023-08-12 16:17:38 +0900

    module state - naive approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
 // Assumes that this is a global state
 let count = 0;
+
+// We must trigger re-rendering consistently.
+// Naive approach: having setState functions at the module level
+const setStateFunctions = new Set<Dispatch<SetStateAction<number>>>();
 
 function Comp1() {
   const [state, setState] = useState(count);
   const inc = () => {
     count += 1;
-    setState(count);
+    setStateFunctions.forEach((setState) => {
+      setState(count);
+    });
   };
+
+  useEffect(() => {
+    setStateFunctions.add(setState);
+    return () => {
+      setStateFunctions.delete(setState);
+    };
+  }, []);
   return (
     <section style={{ margin: 16, padding: 8, color: "skyblue" }}>
       <h2>Comp1</h2>
@@ -21,8 +35,17 @@ function Comp2() {
   const [state, setState] = useState(count);
   const inc = () => {
     count += 2;
-    setState(count);
+    setStateFunctions.forEach((setState) => {
+      setState(count);
+    });
   };
+
+  useEffect(() => {
+    setStateFunctions.add(setState);
+    return () => {
+      setStateFunctions.delete(setState);
+    };
+  }, []);
   return (
     <section style={{ margin: 16, padding: 8, color: "hotpink" }}>
       <h2>Comp2</h2>
